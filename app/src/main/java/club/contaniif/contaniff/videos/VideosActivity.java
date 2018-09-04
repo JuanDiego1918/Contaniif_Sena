57ee517484a649b24f624cac61abd668e4a2fb45
juan <juandiegoz1918@gmail.com>
2018-09-04 13:51:29 -0500

    Juan

---
+++ app/src/main/java/club/contaniif/contaniff/videos/VideosActivity.java
@@ -0,0 +1,15 @@
+package club.contaniif.contaniff.videos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import club.contaniif.contaniff.R;
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+    }
+}
