fc9724f99bdc4ee27d2a5480017616ab9d678d40
juan <juandiegoz1918@gmail.com>
2018-09-04 15:00:55 -0500

    Juan

---
+++ app/src/main/java/club/contaniif/contaniff/actividades/ActivityContenedora.java
@@ -0,0 +1,15 @@
+package club.contaniif.contaniff.actividades;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import club.contaniif.contaniff.R;
+
+public class ActivityContenedora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contenedora);
+    }
+}
--- app/src/main/java/club/contaniif/contaniff/interfaces/AllFragments.java
+++ app/src/main/java/club/contaniif/contaniff/interfaces/AllFragments.java
@@ -1,22 +1,9 @@
 package club.contaniif.contaniff.interfaces;
 
 
-import com.example.juandiego.contaniif.acercade.AcercaDeNosotros;
-import com.example.juandiego.contaniif.acercade.MisionVision;
-import com.example.juandiego.contaniif.acercade.Pantalla_acercade;
-import com.example.juandiego.contaniif.configuracion.PantallaConfiguracion;
-import com.example.juandiego.contaniif.mi_rendimiento.MiRendimiento;
-import com.example.juandiego.contaniif.principal.PantallaPrincipal;
-import com.example.juandiego.contaniif.principal.Pantalla_empezar;
-import com.example.juandiego.contaniif.principal.Pantalla_empezar_drag;
-import com.example.juandiego.contaniif.principal.PrimerFragment;
-import com.example.juandiego.contaniif.principal.SinConexionInternet;
-import com.example.juandiego.contaniif.registro.Registro;
-import com.example.juandiego.contaniif.teoria.Pantalla_teoria;
-import com.example.juandiego.contaniif.videos.CategoriasVideosFragment;
 
 public interface AllFragments
-        extends Pantalla_empezar.OnFragmentInteractionListener
+        /*extends Pantalla_empezar.OnFragmentInteractionListener
         ,Pantalla_teoria.OnFragmentInteractionListener
         ,Pantalla_acercade.OnFragmentInteractionListener,
         PantallaPrincipal.OnFragmentInteractionListener,
@@ -28,5 +15,5 @@ public interface AllFragments
         PantallaConfiguracion.OnFragmentInteractionListener,
         MiRendimiento.OnFragmentInteractionListener,
         Pantalla_empezar_drag.OnFragmentInteractionListener,
-        Registro.OnFragmentInteractionListener{
+        Registro.OnFragmentInteractionListener*/{
 }
