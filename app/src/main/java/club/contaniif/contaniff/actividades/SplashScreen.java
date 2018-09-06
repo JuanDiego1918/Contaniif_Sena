e71c45f7a40ad1323f1fefd38f616ce9dd86afdc
victor1024 <vmgarciahurtado@mail.com>
2018-09-06 11:06:50 -0500

    victor

--- app/src/main/java/club/contaniif/contaniff/actividades/SplashScreen.java
+++ app/src/main/java/club/contaniif/contaniff/actividades/SplashScreen.java
@@ -1,6 +1,9 @@
 package club.contaniif.contaniff.actividades;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -17,6 +20,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        ConnectivityManager con = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = con.getActiveNetworkInfo();
+
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+        } else {
+
+        }
 
         tiempo=new CountDownTimer(2000,1000) {
             @Override
@@ -26,6 +38,7 @@ public class SplashScreen extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+
                 Intent miIntent=new Intent(SplashScreen.this,MainActivity.class);
                 startActivity(miIntent);
                 finish();
