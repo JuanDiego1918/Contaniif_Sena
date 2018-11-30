d4c4de10c8dc72c3174a347865d165250cb0a6b1
Juan <juandiegoz1918@gmail.com>
2018-11-30 10:57:51 -0500

    Juan

--- app/src/main/java/club/contaniif/contaniff/acercaDe/AcercaDeFragment.java
+++ app/src/main/java/club/contaniif/contaniff/acercaDe/AcercaDeFragment.java
@@ -37,7 +37,9 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Use the {@link AcercaDeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class AcercaDeFragment extends Fragment {
+    //Hola
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
